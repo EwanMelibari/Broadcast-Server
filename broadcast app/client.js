1072e7b22dc25686f878693db22e0358516dbae1
Ewan Melibari <ewanhasan2004@gmail.com>
2025-08-13 14:26:38 +0300

    show meny function

--- broadcast app/client.js
+++ broadcast app/client.js
@@ -2,3 +2,12 @@ const net = requier('net');
 const readline = require('readline');
 
 const PORT = 8080;
+
+function showMenu(){
+    console.log("<======= Broadcast App =======>");
+    console.log("1. Create channel");
+    console.log("2. Join channel");
+    console.log("3. Show notifications");
+    console.log("4. Exit");
+    console.log("<=============================>");
+}
